f38c88757edcd6369d54d45cfa391bdaa69620ea
logiflo <logiflo5@gmail.com>
2020-07-05 11:41:02 +0100

    Increase speed up with a new fleet

--- AlienInvasion/game_functions.py
+++ AlienInvasion/game_functions.py
@@ -27,7 +27,7 @@ def check_KEYUP(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stat, ship, aliens, bullets, play_button):
     """Respond to keypresses and mouse events
     """
     for event in pygame.event.get():
@@ -41,10 +41,37 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_KEYUP(event, ship)
 
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(ai_settings, screen ,stat, ship, aliens, bullets, play_button, mouse_x, mouse_y)
+
     return True
 
 
-def draw_screen(ai_settings, screen, ship, aliens, bullets):
+def check_play_button(ai_settings, screen ,stat, ship, aliens, bullets, play_button, mouse_x, mouse_y):
+    """Start a new game when the user clicks play
+    """
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if  button_clicked and not stat.game_active:
+        # Reset the settings
+        ai_settings.initialize_dynamic_settings()
+
+        # Hide the mouse cursor
+        pygame.mouse.set_visible(False)
+        # Reset the game statistisc
+        stat.reset_stat()
+        stat.game_active = True
+
+        # Empty the list of aliens and bullets
+        bullets.empty()
+        aliens.empty()
+
+        # Create a new fleet
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.ship_center()
+
+
+def draw_screen(ai_settings, screen, stat, ship, aliens, bullets, play_button):
     """Update image screen and flip to the new screen
     """
     # Redrawn the screen during each pass through the loop.
@@ -56,6 +83,10 @@ def draw_screen(ai_settings, screen, ship, aliens, bullets):
 
     ship.blitme()
     aliens.draw(screen)
+
+    # Draw the play button is the game is unactive
+    if not stat.game_active:
+        play_button.draw_button()
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
@@ -79,6 +110,7 @@ def check_bullets_colisions(ai_settings, screen, ship, aliens, bullets):
     if len(aliens) == 0:
         # Destroy existing bullets and create a new fleet
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 
@@ -181,6 +213,7 @@ def ship_hit(ai_settings, stat, screen, ship, aliens, bullets):
 
     else:
         stat.game_active = False
+        pygame.mouse.set_visible(True)
 
 
 def check_aliens_bottom(ai_settings, stat, screen, ship, aliens, bullets):
--- AlienInvasion/settings.py
+++ AlienInvasion/settings.py
@@ -8,18 +8,33 @@ class Settings ():
         self.bg_color = (0, 0, 100)
 
         # Ship settings
-        self.ship_speed_factor = 1.5
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (230, 0, 0)
         self.bullet_allowed = 3
 
         # Alien settings
+        self.fleet_drop_speed = 10
+
+        # How quickly the game speed up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
         self.alien_speed_factor = 1
-        self.fleet_drop_speed = 50
+
         # 1 represent right and -1 represent left
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase the speed settings
+        """
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
