4b78acc67569addecf9dcfa35e143abde173c35d
logiflo <logiflo5@gmail.com>
2020-08-05 16:40:31 +0100

    Provides the ability to add new topics

--- LearningLog/learning_logs/views.py
+++ LearningLog/learning_logs/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Topic
+from .forms import TopicForm
+
 
 def index(request):
     """The home page for Learning Log
@@ -24,3 +28,19 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+
+def new_topic(request):
+    """Add a new topic
+    """
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
