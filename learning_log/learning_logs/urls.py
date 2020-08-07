3ae0ef9e64e9feea2fb7d0910d546d395766424d
Lorena Gil <67319308+logiflo@users.noreply.github.com>
2020-08-07 10:47:57 +0100

    Learning log (#3)
    * First files
    
    * Hide SECRET_KEY
    
    * Model definition - class Topic
    
    * Setting up a Superuser as admin
    
    * Model defining - class Entry
    
    * Pages building - Home, Topics and Entries for a single topic
    
    * Provides the ability to add new topics
    
    * Provides the posibility to add and edit a entry
    
    * User implementation - Log in, Log out and Register
    
    * Associate data to the users.
    
    * App styling
    
    * Requeriments
    
    * Heroku implementation

---
+++ learning_log/learning_logs/urls.py
@@ -0,0 +1,29 @@
+"""Defines URL patterns for learning_logs.
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #Home page
+    path('', views.index, name='index'),
+
+    #Topic page : show all topics
+    path('topics/', views.topics, name='topics'),
+
+    #Detail page for a single topic
+    path('topics/<topic_id>/', views.topic, name='topic'),
+
+    #Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    #Page for adding a new entry
+    path('new_entry/<topic_id>', views.new_entry, name='new_entry'),
+
+    #Page for editing an entry
+    path('edit_entry/<entry_id>', views.edit_entry, name='edit_entry'),
+
+    #Page for a removed entry
+    path('remove_entry/<entry_id>', views.remove_entry, name='remove_entry')
+]
+
